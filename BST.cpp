bfd29310fcd686a618d7bda9b1d8faaf2d048c2a
Mary Smith-Gagnon <msmithga@unca.edu>
2012-11-02 14:48:32 -0400

    worked on printTree function

--- BST.cpp
+++ BST.cpp
@@ -69,8 +69,82 @@ void BST<T>::remove(T v) {
 
 template <typename T>
 void BST<T>::printTree() {
+  std::list<Node<T>* > s;
 
+  std::vector<std::vector<bool> > isPresent;
+  std::vector<std::vector<T> > levels;
+  std::vector<T> someRows;
+  std::vector<bool> someOtherRow;
 
+  levels.push_back(someRows);
+  isPresent.push_back(someOtherRow);
+
+  levels[0].push_back(root->getValue());
+  isPresent[0].push_back(true);
+
+  s.push_front(root);
+
+  int currentLevel = 1;
+  int nextLevel = 0;
+  int depth = 1;
+
+  while(!s.empty()) {
+    Node<T>* val = s.front();
+    s.pop_front();
+
+    currentLevel--;
+
+    std::vector<T> row;
+    std::vector<bool> otherRow;
+
+    levels.push_back(row);
+    isPresent.push_back(otherRow);
+
+    if(val->getLeftChild() != 0) {
+      s.push_back(val->getLeftChild());
+      levels[depth].push_back(val->getLeftChild()->getValue());
+      isPresent[depth].push_back(true);
+      nextLevel++;
+    } else {
+      isPresent[depth].push_back(false);
+    }
+
+    if(val->getRightChild() != 0) {
+      s.push_back(val->getRightChild());
+      levels[depth].push_back(val->getRightChild()->getValue());
+      isPresent[depth].push_back(true);
+      nextLevel++;
+    } else {
+      isPresent[depth].push_back(false);
+    }
+
+    if(currentLevel == 0) {
+      depth++;
+      currentLevel = nextLevel;
+      nextLevel = 0;
+    }
+  }
+
+  int width;
+
+  for(int i = 0; i < depth; ++i) {
+    width = pow(1.625,(depth-i-1));
+
+    if(isPresent[i][0]) {
+      std::cout << std::setw(width) << levels[i][0];
+    } else {
+      std::cout << std::setw(width) << " ";
+    }
+
+    for(int j = 1; j <(int)levels[i].size(); ++j) {
+      if(isPresent[i][j]) {
+	std::cout << std::setw(pow(1.75, depth-i-1)) << std::setfill(' ') << levels[i][j];
+      } else {
+	std::cout << std::setw(pow(1.75, depth-i-1)) << std::setfill(' ') << " ";
+      }
+    }
+    std::cout << std::endl;
+  }
 }
 
 
